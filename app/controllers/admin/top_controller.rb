08ad1ddeaf9a50e440fe3c40861993e290197c68
sugimite <sssusuumomo@gmail.com>
2023-12-19 19:30:37 +0900

    管理者seed投入とトップページログインリンク

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,2 +1,5 @@
-class Admin::TopController < ApplicationController
+class Admin::TopController < Admin::Base
+  def index
+    render action: "index"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_15_065903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_administrators_on_code", unique: true
+  end
 
 end
