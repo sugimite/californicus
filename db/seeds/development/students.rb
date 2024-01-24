2b35a30b3368d9528f7fe54512d5f36c4ca7ac1e
sugimite <sssusuumomo@gmail.com>
2024-01-24 18:21:37 +0900

    生徒一覧とシード

---
+++ db/seeds/development/students.rb
@@ -0,0 +1,9 @@
+Student.create!(
+  name: "山田 太郎",
+  name_kana: "ヤマダ タロウ",
+  birthday: "1990/04/27",
+  registration_date: Date.today,
+  cancellation_date: nil,
+  password: "password",
+  email: "taro@example.com"
+)
