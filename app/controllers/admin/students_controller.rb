056687bb10cc207a55b9e548f3282339d4227dc0
sugimite <sssusuumomo@gmail.com>
2024-01-25 18:59:51 +0900

    生徒登録フォームno
    git commit -m 生徒登録フォームno

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -2,4 +2,13 @@ class Admin::StudentsController < Admin::Base
   def index
     @students = Student.order(:name_kana)
   end
+
+  def show
+    @student = Studnet.find(params[:id])
+    redirect_to [ :edit,  :admin, student ]
+  end
+
+  def new
+    @student = Student.new
+  end
 end
