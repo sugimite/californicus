b9b8e54c7344de6a2a2d830704333294e195f8ec
sugimite <sssusuumomo@gmail.com>
2024-02-06 18:44:53 +0900

    名前とメールバリデーション

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,12 @@
 class Student < ApplicationRecord
+  include StringNormalizer
+
+  before_validation do
+    self.name = normalize_as_name(name)
+    self.name_kana = normalize_as_kana(name_kana)
+    self.email = normalize_as_email(email)
+  end
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.password_digest = BCrypt::Password.create(raw_password)
@@ -6,4 +14,11 @@ class Student < ApplicationRecord
       self.password_digest = nil
     end
   end
+
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :name, presence: true
+  validates :name_kana, presence: true,
+    format: { with: KATAKANA_REGEXP, allow_blank: true }
+  validates :email, uniqueness: { sensitive: false }
 end
