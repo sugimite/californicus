95c1051a049ba3996af53da3868587b1de23a80b
sugimite <sssusuumomo@gmail.com>
2024-02-29 19:56:43 +0900

    ボッチ演算子追加と空行削除

--- app/services/student/authenticator.rb
+++ app/services/student/authenticator.rb
@@ -4,8 +4,7 @@ class Student::Authenticator
   end
 
   def authenticate(raw_password)
-    @student &&
-      @student.password_digest &&
+    @student&.password_digest &&
       BCrypt::Password.new(@student.password_digest) == raw_password
   end
 end
