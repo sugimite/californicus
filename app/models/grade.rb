ed948c45942ed0d1abedc127b73de9bd893af35a
sugimite <sssusuumomo@gmail.com>
2024-03-26 21:31:20 +0900

    バリデーションの追加等

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,14 @@
 class Grade < ApplicationRecord
   belongs_to :student
+
+  SUBJECT_TYPES = %w[ 英語 数学 国語 社会 理科].freeze
+
+  validates :id, uniqueness: { scope: :student_id }
+  validates :student_id, presence: true
+  validates :year, presence: true
+  validates :score, presence: true
+  validates :score, numericality: {in: 0..100}
+  validates :subject_type, presence: true
+  validates :subject_type, inclusion: {in: SUBJECT_TYPES, allow_blank: true}
+  validates :test_type, presence: true
 end
--- db/seeds/development/grades.rb
+++ db/seeds/development/grades.rb
@@ -1,10 +1,9 @@
 students = Student.all
-subject_types = [ "英語", "数学", "国語", "社会", "理科" ]
+subject_types = Grade::SUBJECT_TYPES
 test_types = [ "1学期中間", "1学期期末", "2学期中間", "2学期期末", "学年末", "実力1", "実力2", "実力3", "実力4", "実力5"]
 years = [ 2024, 2023, 2022 ]
 
 150.times do |n|
-
   Grade.create!(
     student: students.sample,
     year: years[n % 3],
