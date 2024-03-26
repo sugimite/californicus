6f0091d09ef16e396b33ff9978e7d4c0fd040e6c
sugimite <sssusuumomo@gmail.com>
2024-03-26 21:31:20 +0900

    バリデーションの追加等

--- db/migrate/20240305100712_create_grades.rb
+++ db/migrate/20240305100712_create_grades.rb
@@ -1,7 +1,6 @@
 class CreateGrades < ActiveRecord::Migration[7.0]
   def change
     create_table :grades do |t|
-
       t.references :student, null: false, index: false, foreign_key: true
       t.integer :year, null: false 
       t.integer :score, null: false
