428a6942ab1c914e1027d1cc6b247270a4f0ab0a
sugimite <sssusuumomo@gmail.com>
2024-05-08 21:34:33 +0900

    バリデーションの修正

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,7 +3,7 @@ class Attendance < ApplicationRecord
   belongs_to :student
 
   before_save :caliculate_staying_time
-  before_save :set_out_at_to_nil_if_same_as_in_at
+  before_validation :set_out_at_to_nil_if_same_as_in_at
 
   private 
   def caliculate_staying_time
@@ -19,9 +19,19 @@ class Attendance < ApplicationRecord
     self.out_at = nil if in_at == out_at
   end
 
+  validate :out_at_should_be_later_than_in_at
+
+  private
+
+  def out_at_should_be_later_than_in_at
+    if out_at && in_at && out_at <= in_at
+      errors.add(:out_at, "離席時間は出席時間よりも遅くなければなりません")
+    end
+  end
+  
 
   validates :administrator_id, presence: true
   validates :student_id,  presence: true
   validates :attended_date, presence: true
-
+  validates :in_at, presence: true
 end
