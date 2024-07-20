1fdfcc68022d6ee645a8f27a581425d07ac5563e
sugimite <sssusuumomo@gmail.com>
2024-07-20 15:56:06 +0900

    メッセージ機能の修正（送信直後位置、エリアクリア）

--- app/controllers/student/contacts_controller.rb
+++ app/controllers/student/contacts_controller.rb
@@ -1,6 +1,6 @@
 class Student::ContactsController < Student::Base
   def index
-    @contacts = Contact.all
+    @contacts = @student.contacts.order(date: :asc)
     @new_contact = Contact.new
   end
 
@@ -9,12 +9,12 @@ def create
   
   if @contact.save
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.append('contacts', partial: 'student/contacts/contact', locals: { contact: @contact }) }
+      format.turbo_stream 
       format.html { redirect_to contacts_path, notice: 'メッセージが送信されました。' }
     end
   else
     @contacts = Contact.all
-    @new_contact = @contact
+    @new_contact = Contact.new
     render :index, status: :unprocessable_entity
   end
 end
