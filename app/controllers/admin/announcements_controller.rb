8bbab807693fab5ee39bd8bb062c796b3b77aa81
sugimite <sssusuumomo@gmail.com>
2024-08-07 20:05:01 +0900

    修正と削除

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -8,10 +8,44 @@ class Admin::AnnouncementsController < Admin::Base
   end
 
   def new
-    @announcement =Announcement.new
+    @announcement = Announcement.new
   end
 
   def edit
     @announcement = Announcement.find(params[:id])
+  end
+
+  def create
+    @announcement = Announcement.new(announcement_params)
+
+    if @announcement.save
+      flash.notice = "お知らせを掲載しました。"
+      redirect_to :admin_announcements
+    else
+      render action: "new", status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @announcement = Announcement.find(params[:id])
+    @announcement.assign_attributes(announcement_params)
+
+    if @announcement.save
+      flash.notice = "お知らせを更新しました。"
+      redirect_to :admin_announcements
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Announcement.find(params[:id]).destroy!
+    redirect_to :admin_announcements, notice: "お知らせを削除しました。"
+  end
+
+  private def announcement_params
+    params.require(:announcement).permit(
+      :administrator_id, :title, :content, :start_date, :end_date 
+    )
   end
 end
