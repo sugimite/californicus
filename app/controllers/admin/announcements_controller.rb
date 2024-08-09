59d2c6c9237f9e1f8e30a34626e61615d784afc3
sugimite <sssusuumomo@gmail.com>
2024-08-09 15:44:21 +0900

    公開範囲と学年設定

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AnnouncementsController < Admin::Base
   def index
     @announcements = Announcement.order(start_date: :desc)
-  end
+  end  
 
   def show
     @announcement = Announcement.find(params[:id])
@@ -19,6 +19,7 @@ class Admin::AnnouncementsController < Admin::Base
     @announcement = Announcement.new(announcement_params)
 
     if @announcement.save
+      associate_students_with_announcement
       flash.notice = "お知らせを掲載しました。"
       redirect_to :admin_announcements
     else
@@ -31,6 +32,8 @@ class Admin::AnnouncementsController < Admin::Base
     @announcement.assign_attributes(announcement_params)
 
     if @announcement.save
+      @announcement.announcement_students.destroy_all
+      associate_students_with_announcement
       flash.notice = "お知らせを更新しました。"
       redirect_to :admin_announcements
     else
@@ -43,9 +46,22 @@ class Admin::AnnouncementsController < Admin::Base
     redirect_to :admin_announcements, notice: "お知らせを削除しました。"
   end
 
-  private def announcement_params
+  private
+
+  def associate_students_with_announcement
+    selected_grades = params[:announcement][:school_grades].reject(&:blank?).map(&:to_i)
+
+    students_to_associate = if selected_grades.empty?
+                              Student.all
+                            else
+                              Student.where(birthday: selected_grades.map { |grade| grade.years.ago.beginning_of_year..grade.years.ago.end_of_year })
+                            end
+    @announcement.announcement_students.create!(students_to_associate.map { |student| { student_id: student.id } })
+  end
+
+  def announcement_params
     params.require(:announcement).permit(
-      :administrator_id, :title, :content, :start_date, :end_date 
+      :administrator_id, :title, :content, :start_date, :end_date, :ages
     )
   end
 end
