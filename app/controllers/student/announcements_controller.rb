167ec271fb9401a8c8d76e08ec97d5a51820c405
sugimite <sssusuumomo@gmail.com>
2024-08-13 12:08:02 +0900

    生徒側での表示と修正時のデフォルト日時修正

---
+++ app/controllers/student/announcements_controller.rb
@@ -0,0 +1,12 @@
+class Student::AnnouncementsController < Student::Base
+  def index
+    student = current_student
+    today = Date.today
+
+    # 生徒に関連するアナウンスメントと全体公開のアナウンスメントを取得
+    @announcements = Announcement.left_joins(:announcement_students)
+                                 .where("announcement_students.student_id = ? OR announcement_students.id IS NULL", student.id)
+                                 .where("announcements.end_date IS NULL OR announcements.end_date >= ?", today)
+                                 .distinct
+  end
+end
