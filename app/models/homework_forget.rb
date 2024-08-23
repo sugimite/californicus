d3e8bdcac3c523ab31c7ad56c4023b18bc312c17
sugimite <sssusuumomo@gmail.com>
2024-08-23 17:23:30 +0900

    年度毎の記録

--- app/models/homework_forget.rb
+++ app/models/homework_forget.rb
@@ -3,4 +3,14 @@ class HomeworkForget < ApplicationRecord
 
   validates :forgetted_on, presence: true
   validates :count, numericality: { greater_than_or_equal_to: 1 }
+
+  private
+
+  def determine_school_year(forgetted_on)
+    if forgetted_on.month >= 3
+      forgetted_on.year
+    else
+      forgetted_on.year - 1
+    end
+  end
 end
