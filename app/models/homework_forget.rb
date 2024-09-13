2e15b7f19cd2ac4f4bb3d395b60d3a09836b97fc
sugimite <sssusuumomo@gmail.com>
2024-09-13 19:51:24 +0900

    messageの装飾と宿題カウントの修正

--- app/models/homework_forget.rb
+++ app/models/homework_forget.rb
@@ -4,7 +4,13 @@ class HomeworkForget < ApplicationRecord
   validates :forgetted_on, presence: true
   validates :count, numericality: { greater_than_or_equal_to: 1 }
 
+  before_save :set_school_year
+
   private
+
+  def set_school_year
+    self.school_year = determine_school_year(forgetted_on)
+  end
 
   def determine_school_year(forgetted_on)
     if forgetted_on.month >= 3
